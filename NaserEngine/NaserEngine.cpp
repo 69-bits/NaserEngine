8f657df60ec99edee7e60915d05c94f58f767a35
69-bits <jhon.alejandro0245@gmail.com>
2025-02-28 11:59:36 -0600

    Implementación de buffers
    - Index, Vertex, Constant buffers agregados

--- NaserEngine/NaserEngine.cpp
+++ NaserEngine/NaserEngine.cpp
@@ -7,8 +7,10 @@
 #include "RenderTargetView.h"
 #include "DepthStencilView.h"
 #include "Viewport.h"
-//#include "InputLayout.h"
 #include "ShaderProgram.h"
+#include "Buffer.h"
+#include "MeshComponent.h"
+
 // Global Variables
 Window															g_window;
 Device															g_device;
@@ -19,12 +21,14 @@ Textura															g_depthStencil;
 RenderTargetView										g_renderTargetView;
 DepthStencilView										g_depthStencilView;
 Viewport														g_viewport;
-//InputLayout													g_inputLayout;
 ShaderProgram												g_shaderProgram;
+Buffer                              g_vertexBuffer;
+Buffer                              g_indexBuffer;
+MeshComponent                       g_mesh;
+Buffer                              g_neverChange;
+Buffer                              g_changeOnResize;
+Buffer                              g_changesEveryFrame;
 
-//ID3D11VertexShader*									g_pVertexShader = nullptr;
-//ID3D11PixelShader*									g_pPixelShader = nullptr;
-//ID3D11InputLayout*									g_pVertexLayout = nullptr;
 ID3D11Buffer* g_pVertexBuffer = nullptr;
 ID3D11Buffer* g_pIndexBuffer = nullptr;
 ID3D11Buffer* g_pCBNeverChanges = nullptr;
@@ -159,7 +163,7 @@ InitDevice() {
 	texcoord.InstanceDataStepRate = 0;
 	Layout.push_back(texcoord);
 
-	//// Create the Shader Program
+	// Create the Shader Program
 	hr = g_shaderProgram.init(g_device, "NaserEngine.fx", Layout);
 
 	if (FAILED(hr))
@@ -199,7 +203,7 @@ InitDevice() {
 				{ XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT2(0.0f, 1.0f) },
 	};
 
-	D3D11_BUFFER_DESC bd;
+	/*D3D11_BUFFER_DESC bd;
 	ZeroMemory(&bd, sizeof(bd));
 	bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.ByteWidth = sizeof(SimpleVertex) * 24;
@@ -210,12 +214,8 @@ InitDevice() {
 	InitData.pSysMem = vertices;
 	hr = g_device.CreateBuffer(&bd, &InitData, &g_pVertexBuffer);
 	if (FAILED(hr))
-		return hr;
+		return hr;*/
 
-	// Set vertex buffer
-
-
-	// Create index buffer
 	// Create vertex buffer
 	WORD
 		indices[] = {
@@ -238,30 +238,67 @@ InitDevice() {
 				23,20,22
 	};
 
-	bd.Usage = D3D11_USAGE_DEFAULT;
+	/*bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.ByteWidth = sizeof(WORD) * 36;
 	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
 	bd.CPUAccessFlags = 0;
 	InitData.pSysMem = indices;
 	hr = g_device.CreateBuffer(&bd, &InitData, &g_pIndexBuffer);
 	if (FAILED(hr))
+		return hr;*/
+
+	for (SimpleVertex vertex : vertices) {
+    g_mesh.m_vertex.push_back(vertex);
+	}
+
+	for (unsigned int index : indices) {
+    g_mesh.m_index.push_back(index);
+	}
+
+  g_mesh.m_numVertex = g_mesh.m_vertex.size();
+  g_mesh.m_numIndex = g_mesh.m_index.size();
+
+	hr = g_vertexBuffer.init(g_device, g_mesh, D3D11_BIND_VERTEX_BUFFER);
+	if (FAILED(hr)) {
+		ERROR("VertexBuffer", "Resize", "Failed to create new Buffer");
 		return hr;
+	}
 
-	// Set index buffer
-
-	// Set primitive topology
-
+	hr = g_indexBuffer.init(g_device, g_mesh, D3D11_BIND_INDEX_BUFFER);
+	if (FAILED(hr)) {
+		ERROR("IndexBuffer", "Resize", "Failed to create new Buffer");
+		return hr;
+	}
 
 	// Create the constant buffers
-	bd.Usage = D3D11_USAGE_DEFAULT;
+
+	/*bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.ByteWidth = sizeof(CBNeverChanges);
 	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
 	bd.CPUAccessFlags = 0;
 	hr = g_device.CreateBuffer(&bd, nullptr, &g_pCBNeverChanges);
 	if (FAILED(hr))
-		return hr;
+		return hr;*/
 
-	bd.ByteWidth = sizeof(CBChangeOnResize);
+  hr = g_neverChange.init(g_device, sizeof(CBNeverChanges));
+  if (FAILED(hr)) {
+    ERROR("Buffer", "init", "Failed to create new Buffer");
+    return hr;
+  }
+
+  hr = g_changeOnResize.init(g_device, sizeof(CBChangeOnResize));
+  if (FAILED(hr)) {
+    ERROR("Buffer", "init", "Failed to create new Buffer");
+    return hr;
+  }
+
+  hr = g_changesEveryFrame.init(g_device, sizeof(CBChangesEveryFrame));
+  if (FAILED(hr)) {
+    ERROR("Buffer", "init", "Failed to create new Buffer");
+    return hr;
+  }
+
+	/*bd.ByteWidth = sizeof(CBChangeOnResize);
 	hr = g_device.CreateBuffer(&bd, nullptr, &g_pCBChangeOnResize);
 	if (FAILED(hr))
 		return hr;
@@ -269,7 +306,7 @@ InitDevice() {
 	bd.ByteWidth = sizeof(CBChangesEveryFrame);
 	hr = g_device.CreateBuffer(&bd, nullptr, &g_pCBChangesEveryFrame);
 	if (FAILED(hr))
-		return hr;
+		return hr;*/
 
 	// Load the Texture
 	hr = D3DX11CreateShaderResourceViewFromFile(g_device.m_device, "seafloor.dds", nullptr, nullptr, &g_pTextureRV, nullptr);
@@ -463,18 +500,25 @@ void update() {
 	// Actualizar el buffer constante del frame
 	cb.mWorld = XMMatrixTranspose(g_World);
 	cb.vMeshColor = g_vMeshColor;
-	g_deviceContext.UpdateSubresource(g_pCBChangesEveryFrame, 0, nullptr, &cb, 0, 0);
+	//g_deviceContext.UpdateSubresource(g_pCBChangesEveryFrame, 0, nullptr, &cb, 0, 0);
+
+	g_changesEveryFrame.update(g_deviceContext, 0, nullptr, &cb, 0, 0);
+
+
 
 	// Actualizar la matriz de proyecci�n
 	g_Projection = XMMatrixPerspectiveFovLH(XM_PIDIV4, g_window.m_width / (float)g_window.m_height, 0.01f, 100.0f);
 
 	// Actualizar la vista (si es necesario cambiar din�micamente)
 	cbNeverChanges.mView = XMMatrixTranspose(g_View);
-	g_deviceContext.UpdateSubresource(g_pCBNeverChanges, 0, nullptr, &cbNeverChanges, 0, 0);
+	
+	g_neverChange.update(g_deviceContext, 0, nullptr, &cbNeverChanges, 0, 0);
+	//g_deviceContext.UpdateSubresource(g_pCBNeverChanges, 0, nullptr, &cbNeverChanges, 0, 0);
 
 	// Actualizar la proyecci�n en el buffer constante
 	cbChangesOnResize.mProjection = XMMatrixTranspose(g_Projection);
-	g_deviceContext.UpdateSubresource(g_pCBChangeOnResize, 0, nullptr, &cbChangesOnResize, 0, 0);
+  g_changeOnResize.update(g_deviceContext, 0, nullptr, &cbChangesOnResize, 0, 0);
+	//g_deviceContext.UpdateSubresource(g_pCBChangeOnResize, 0, nullptr, &cbChangesOnResize, 0, 0);
 }
 
 //--------------------------------------------------------------------------------------
@@ -496,16 +540,25 @@ void Render() {
 	// Configurar los buffers y shaders para el pipeline
 	g_shaderProgram.render(g_deviceContext);
 
-	g_deviceContext.IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
-	g_deviceContext.IASetIndexBuffer(g_pIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+	/*g_deviceContext.IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+	g_deviceContext.IASetIndexBuffer(g_pIndexBuffer, DXGI_FORMAT_R16_UINT, 0);*/
+
+	g_vertexBuffer.render(g_deviceContext, 0, 1);
+	g_indexBuffer.render(g_deviceContext, 0, 1, DXGI_FORMAT_R32_UINT);
+
 	g_deviceContext.IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	// Asignar shaders y buffers constantes
-	g_deviceContext.VSSetConstantBuffers(0, 1, &g_pCBNeverChanges);
+	/*g_deviceContext.VSSetConstantBuffers(0, 1, &g_pCBNeverChanges);
 	g_deviceContext.VSSetConstantBuffers(1, 1, &g_pCBChangeOnResize);
-	g_deviceContext.VSSetConstantBuffers(2, 1, &g_pCBChangesEveryFrame);
+	g_deviceContext.VSSetConstantBuffers(2, 1, &g_pCBChangesEveryFrame);*/
 
-	g_deviceContext.PSSetConstantBuffers(2, 1, &g_pCBChangesEveryFrame);
+	g_neverChange.render(g_deviceContext, 0, 1);
+  g_changeOnResize.render(g_deviceContext, 1, 1);
+  g_changesEveryFrame.render(g_deviceContext, 2, 1);
+
+	//g_deviceContext.PSSetConstantBuffers(2, 1, &g_pCBChangesEveryFrame);
+  g_changesEveryFrame.render(g_deviceContext, 2, 1, DXGI_FORMAT_UNKNOWN, true);
 	g_deviceContext.PSSetShaderResources(0, 1, &g_pTextureRV);
 	g_deviceContext.PSSetSamplers(0, 1, &g_pSamplerLinear);
 
