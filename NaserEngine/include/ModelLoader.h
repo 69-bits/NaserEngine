0e0bda09decd5c1855e0b6ee3e3238ec3bcce26c
69-bits <jhon.alejandro0245@gmail.com>
2025-03-28 02:24:44 -0600

    fbx directorio 2

---
+++ NaserEngine/include/ModelLoader.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Prerequisites.h"
+#include "MeshComponent.h"
+#include "fbxsdk.h"
+
+class 
+ModelLoader
+{
+public:
+	ModelLoader() = default;
+	~ModelLoader() = default;
+
+  bool
+  LoadFBX_model(const std::string& filenpath);
+
+  void
+  ProcessFBXNode(FbxNode* node);
+  
+  void
+  ProcessFBXMesh(FbxNode* node);
+
+  void
+  ProcessFBXMaterials(FbxSurfaceMaterial* material);
+
+  std::vector<std::string>
+    GetTextureFileNames() const { return textureFileNames; };
+
+private:
+  FbxManager* lSdkManager;
+  FbxScene* lScene;
+  std::vector<std::string> textureFileNames;
+
+public:
+  std::vector<MeshComponent> m_meshes;
+};
